8a11381b942a47557568dfb42c3c3cac0ab26815
linvanda <1049180458@qq.com>
2017-10-27 22:53:16 +0800

    demo 3

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,8 @@
             <a href="/" id="logo">Sample App</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/help">帮助</a></li>
-                    <li><a href="#">登录</a></li>
+                    <li><a href="{{route('help')}}">帮助</a></li>
+                    <li><a href="{{route('signup')}}">登录</a></li>
                 </ul>
             </nav>
         </div>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('title', 'sign up')
+@section('content')
+注册页
+@stop
