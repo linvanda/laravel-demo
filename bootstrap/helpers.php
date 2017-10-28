0d665a7b38f674bcfb527954d60733f4a21870e1
linvanda <1049180458@qq.com>
2017-10-28 18:46:42 +0800

    heroku使用postgresql

---
+++ bootstrap/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_db_config()
+{
+    // heroku使用postgresql,本地使用mysql
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$dbConfig = get_db_config();
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $dbConfig['connection'],
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +58,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $dbConfig['host'],
+            'port'     => env('DB_PORT', '5432'),
+            'database' => $dbConfig['database'],
+            'username' => $dbConfig['username'],
+            'password' => $dbConfig['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
