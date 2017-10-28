1560e4930d476d6e61c8d75fdfad5e5c9fc36a6e
linvanda <1049180458@qq.com>
2017-10-28 10:08:37 +0800

    article

---
+++ database/migrations/2017_10_28_015738_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 60);
+            $table->text('content');
+            $table->timestamps();
+            $table->enum('deleted', [1, 0])->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
